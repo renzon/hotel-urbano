85a55c50169f574ed6afe9a6cd77451df755d6ad
renzon <renzon@gmail.com>
2016-06-18 12:57:29 -0300

    Atribuição de slice

--- intro/cc.py
+++ intro/cc.py
@@ -3,7 +3,9 @@ cc = list(range(16))
 INICIO = slice(4)
 n = len(cc)
 FIM = slice(- 4, None)
-cc_obfuscado = cc[INICIO] + (['*'] * (n-8)) + cc[FIM]
+mascara = ['*'] * (n - 8)
+cc_obfuscado = cc[INICIO] + mascara + cc[FIM]
 print(cc_obfuscado)
 
-
+cc[4:-4] = mascara
+print(cc)
