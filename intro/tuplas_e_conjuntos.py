b5686f3dd608e6b73f6cb85ccb8bcead003a1204
renzon <renzon@gmail.com>
2016-06-18 16:13:27 -0300

    Removendo repetidos mantendo ordem original

--- intro/tuplas_e_conjuntos.py
+++ intro/tuplas_e_conjuntos.py
@@ -8,6 +8,19 @@ print(cjt)
 pares = {0, 2, 4, 6, 8}
 
 print(cjt.intersection(pares))
-print(cjt-pares)
+print(cjt - pares)
 print(cjt.union(pares))
 print(1 in cjt)
+
+l = [2, 6, 6, 4, 4, 6, 1, 4, 2, 2, 453534535345]
+lookup = set()
+nao_repetidos_na_ordem_original = []
+
+for n in l:
+    if n not in lookup:
+        nao_repetidos_na_ordem_original.append(n)
+        lookup.add(n)
+
+print(nao_repetidos_na_ordem_original)
+
+# [1, 2, 4, 6]
