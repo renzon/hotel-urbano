655f84322689278246c23d80860cfe07bd7ff892
renzon <renzon@gmail.com>
2016-06-18 17:32:54 -0300

    docstest inciao para procura de unicode

---
+++ caracteres/carater_finder.py
@@ -0,0 +1,13 @@
+import unicodedata
+
+def procurar_char(palavra):
+    """
+    Procura caracter que contenha palavra em seu nome
+    :param palavra: string
+    :return: lista com tuplas (caracter, nome do carecter)
+
+    >>> procurar_char('suit')
+    [('♠','BLACK SPADE SUIT'),('♥','RED HEART SUIT'),('♦','RED DIAMOND SUIT'),('♣','BLACK CLUB SUIT'),('♤','WHITE SPADE SUIT'),('♡','WHITE HEART SUIT'),('♢','WHITE DIAMOND SUIT'),('♧','WHITE CLUB SUIT')]
+    """
+    return 5
+
