641dd6482e4e777742837e0288104c7bd7fbb113
renzon <renzon@gmail.com>
2016-06-24 13:34:05 -0300

    Incluindo setup e publicando pacote

--- ghhu/github.py
+++ ghhu/github.py
@@ -1,12 +1,20 @@
+import json
+
 import requests
 
 
 def avatar(nome):
     r = requests.get('https://api.github.com/users/{nome}'.format(nome=nome))
-    js = r.json()
+    js = json.loads(r.text)
     return js['avatar_url']
+
+def repositorios(nome):
+    """
+    Retornar a lista de repos do usuário
+    :param nome: nome do usuário
+    :return: list
+    """
 
 
 if __name__ == '__main__':
     print(avatar('renzon'))
-
