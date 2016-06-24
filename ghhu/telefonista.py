723b87dfb63014f4abb2399840483d55a477a6ba
renzon <renzon@gmail.com>
2016-06-24 14:21:46 -0300

    Problema em teste unitario dependendo de telefone real

---
+++ ghhu/telefonista.py
@@ -0,0 +1,15 @@
+from ghhu.telefone import Telefone
+
+
+class Telefonista:
+    def __init__(self):
+        self._telefone = Telefone()
+        self._contatos = []
+
+    def adicionar_contato(self, nome, numero):
+        self._contatos.append((nome, numero))
+
+    def ligar_para_todos_contatos(self):
+        tel = self._telefone
+        return ['{msg_telefone} - {contato}'.format(msg_telefone=tel.telefonar(numero), contato=nome)
+                for nome, numero in self._contatos]
---
+++ tests/telefonista_tests.py
@@ -0,0 +1,32 @@
+import unittest
+
+from ghhu.telefonista import Telefonista
+
+
+class TelefonistaTestesBasicos(unittest.TestCase):
+    def teste_adicionar_um_contato(self):
+        telefonista = Telefonista()
+        telefonista.adicionar_contato('Renzo', '2345678')
+        self.assertEqual([('Renzo', '2345678')], telefonista._contatos)
+
+    def teste_ligar_para_todos_contatos(self):
+        telefonista = Telefonista()
+        telefonista.adicionar_contato('Renzo', '2345678')
+        ligacao_feita = telefonista.ligar_para_todos_contatos()
+        self.assertListEqual(['Ligando de verdade para 2345678 - Renzo'], ligacao_feita)
+        telefonista.adicionar_contato('Henrique', '8765432')
+        ligacoes_feitas = telefonista.ligar_para_todos_contatos()
+        self.assertListEqual(['Ligando de mentira para 2345678 - Renzo',
+                              'Ligando de mentira para 8765432 - Henrique'], ligacoes_feitas)
+
+
+class TelefonistaTestesIntegracao(unittest.TestCase):
+    def teste_ligar_para_todos_contatos(self):
+        telefonista = Telefonista()
+        telefonista.adicionar_contato('Renzo', '2345678')
+        ligacao_feita = telefonista.ligar_para_todos_contatos()
+        self.assertListEqual(['Ligando de verdade para 2345678 - Renzo'], ligacao_feita)
+        telefonista.adicionar_contato('Henrique', '8765432')
+        ligacoes_feitas = telefonista.ligar_para_todos_contatos()
+        self.assertListEqual(['Ligando de verdade para 2345678 - Renzo',
+                              'Ligando de verdade para 8765432 - Henrique'], ligacoes_feitas)
