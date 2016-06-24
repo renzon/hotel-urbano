6438b538718a418c858cb4c49c84531c9d3d99d7
renzon <renzon@gmail.com>
2016-06-24 14:34:33 -0300

    Injecao de depencia via argumento de métodos

--- ghhu/telefonista.py
+++ ghhu/telefonista.py
@@ -9,7 +9,7 @@ class Telefonista:
     def adicionar_contato(self, nome, numero):
         self._contatos.append((nome, numero))
 
-    def ligar_para_todos_contatos(self):
-        tel = self._telefone
+    def ligar_para_todos_contatos(self, telefone=None):
+        tel = self._telefone if telefone is None else telefone
         return ['{msg_telefone} - {contato}'.format(msg_telefone=tel.telefonar(numero), contato=nome)
                 for nome, numero in self._contatos]
--- tests/telefonista_tests.py
+++ tests/telefonista_tests.py
@@ -10,12 +10,17 @@ class TelefonistaTestesBasicos(unittest.TestCase):
         self.assertEqual([('Renzo', '2345678')], telefonista._contatos)
 
     def teste_ligar_para_todos_contatos(self):
+        class TelefoneMock():
+            def telefonar(self, numero):
+                return 'Ligando de mentira para %s' % numero
+
+        telefone_mock = TelefoneMock()
         telefonista = Telefonista()
         telefonista.adicionar_contato('Renzo', '2345678')
-        ligacao_feita = telefonista.ligar_para_todos_contatos()
-        self.assertListEqual(['Ligando de verdade para 2345678 - Renzo'], ligacao_feita)
+        ligacao_feita = telefonista.ligar_para_todos_contatos(telefone=telefone_mock)
+        self.assertListEqual(['Ligando de mentira para 2345678 - Renzo'], ligacao_feita)
         telefonista.adicionar_contato('Henrique', '8765432')
-        ligacoes_feitas = telefonista.ligar_para_todos_contatos()
+        ligacoes_feitas = telefonista.ligar_para_todos_contatos(telefone=telefone_mock)
         self.assertListEqual(['Ligando de mentira para 2345678 - Renzo',
                               'Ligando de mentira para 8765432 - Henrique'], ligacoes_feitas)
 
