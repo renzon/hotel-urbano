48293574d207b04b269b6a113bfbde6310128b3c
renzon <renzon@gmail.com>
2016-06-24 17:23:36 -0300

    decorator wraps de functools

--- funcoes/flask_poc.py
+++ funcoes/flask_poc.py
@@ -1,3 +1,5 @@
+import functools
+
 _rotas = {}
 
 
@@ -20,10 +22,11 @@ _BD = {ADMIN: ['renzo', 'luis'], MANAGER: ['diego', 'henrique']}
 
 def login_required(group):
     def decorator(funcao):
+        @functools.wraps(funcao)
         def decorada(usuario, *args, **kwargs):
             if usuario in _BD[group]:
                 return funcao(usuario, *args, **kwargs)
-            return 'Usuario {} nao pode acessar funcao {}'.\
+            return 'Usuario {} nao pode acessar funcao {}'. \
                 format(usuario, funcao.__name__)
 
         return decorada
